ef6323aefb42ace56f887382c1f103ddd37f61d2
emmanuelparadis <Emmanuel.Paradis@ird.fr>
2015-02-26 00:25:16 +0100

    complete update for building pegas 0.7

---
+++ pegas/R/zzz.R
@@ -0,0 +1,11 @@
+## zzz.R (2015-02-25)
+
+##   Library Loading
+
+## Copyright 2015 Emmanuel Paradis
+
+## This file is part of the R-package `pegas'.
+## See the file ../COPYING for licensing issues.
+
+.PlotHaploNetEnv <- new.env()
+
