67da1bcee8925d10bea57d4cb05f34ddec2e8802
emmanuelparadis <Emmanuel.Paradis@ird.fr>
2018-04-20 16:04:50 +0200

    fix in Fast()

--- pegas/R/Fst.R
+++ pegas/R/Fst.R
@@ -1,13 +1,13 @@
-## Fst.R (2010-06-09)
+## Fst.R (2018-04-20)
 
 ##   F-Statistics
 
-## Copyright 2009-2010 Emmanuel Paradis
+## Copyright 2009-2018 Emmanuel Paradis
 
 ## This file is part of the R-package `pegas'.
 ## See the file ../DESCRIPTION for licensing issues.
 
-Fst <- function(x, pop = NULL)
+Fst <- function(x, pop = NULL, quiet = TRUE)
 {
     if (any(getPloidy(x) != 2))
         stop("Fst() requires diploid data")
@@ -16,59 +16,58 @@ Fst <- function(x, pop = NULL)
         pop <- x$population
         if (is.null(pop)) stop("no 'population' column in x")
     } else {
-        pop <- if (is.numeric(pop)) x[, pop] else factor(pop)
+        pop <- if (is.numeric(pop) && length(pop) == 1) x[, pop] else factor(pop)
     }
-    r <- length(attr(pop, "levels")) # nb of pops
-    pop <- as.integer(pop)
-    nloci <- length(attr(x, "locicol"))
-    nBYpop <- tabulate(pop)
-    N <- nrow(x)
-    nbar <- N/r
 
-    ## get the frequencies in each population for each locus
-    ALLELES <- getAlleles(x)
-    p <- vector("list", nloci)
-    for (j in 1:nloci)
-        p[[j]] <- matrix(0, r, length(ALLELES[[j]]))
-    h <- p
-    for (i in 1:r) {
-        s <- summary(x[pop == i, ]) # levels are preserved
-        for (j in 1:nloci) {
-            tmp <- s[[j]]
-            p[[j]][i, ] <- tmp$allele
-            allel <- names(tmp$allele)
-            genot <- names(tmp$genotype)
+    LOCI <- attr(x, "locicol")
+    nloci <- length(LOCI)
+    x <- x[, LOCI]
+    x$pop <- factor(pop)
+    ipop <- nloci + 1
+
+    ## 'p' is a matrix with alleles as columns and populations
+    ##    as rows, and its entries are the counts
+    ## 'h' is the same with the number of heterozygotes
+
+    res <- matrix(0, nloci, 3)
+
+    for (j in 1:nloci) {
+        if (!quiet) cat("\rAnalyzing locus", j, "/", nloci)
+        Z <- x[, c(j, ipop)]
+        Z <- na.omit(Z) # all n's are calculated locus-wise (2018-04-20)
+        N <- nrow(Z)
+        nBYpop <- tabulate(Z$pop)
+        r <- length(nBYpop) # number of pops
+        nbar <- N/r
+        nC <- (N - sum(nBYpop^2)/N)/(r - 1)
+        ALLELES <- getAlleles(Z)[[1]]
+        h <- p <- matrix(0, r, length(ALLELES))
+        for (i in 1:r) {
+            s <- summary(Z[as.integer(Z$pop) == i, ])[[1]] # levels are preserved
+            allel <- names(s$allele)
+            genot <- names(s$genotype)
+            p[i, ] <- s$allele
             for (k in seq_along(allel)) {
                 for (l in seq_along(genot)) {
                     ag <- unlist(strsplit(genot[l], "/"))
                     if (sum(ag %in% allel[k]) == 1)
-                        h[[j]][i, k] <- h[[j]][i, k] + tmp$genotype[l]
+                        h[i, k] <- h[i, k] + s$genotype[l]
                 }
             }
         }
-    }
-    ## 'p' is a list with, for each locus, a matrix
-    ##    with alleles as columns and populations as rows,
-    ##    and its entries are the counts
-    ## 'h' is the same but with the number of heterozygotes
-
-    nC <- (N - sum(nBYpop^2)/N)/(r - 1)
-
-    obj <- matrix(0, nloci, 3)
-    for (j in 1:nloci) {
-        ptild <- p[[j]]/(2 * nBYpop)
-        pbar <- colSums(p[[j]])/(2 * N) # for each allele in the locus
+        ptild <- p/(2 * nBYpop)
+        pbar <- colSums(p)/(2 * N) # for each allele in the locus
         s2 <- colSums(nBYpop * (ptild - rep(pbar, each = r))^2)/((r - 1) * nbar)
-        hbar <- colSums(h[[j]])/N # id.
+        hbar <- colSums(h)/N # id.
         A <- pbar * (1 - pbar) - (r - 1) * s2/r
         a <- nbar * (s2 - (A - hbar/4)/(nbar - 1))/nC
         b <- nbar * (A - (2*nbar - 1) * hbar/(4*nbar))/(nbar - 1)
         c <- hbar/2
-        obj[j, 1] <- 1 - sum(c)/sum(a + b + c)
-        obj[j, 2] <- sum(a)/sum(a + b + c)
-        obj[j, 3] <- 1 - sum(c)/sum(b + c)
+        res[j, 1] <- 1 - sum(c)/sum(a + b + c)
+        res[j, 2] <- sum(a)/sum(a + b + c)
+        res[j, 3] <- 1 - sum(c)/sum(b + c)
     }
-    dimnames(obj) <- list(names(x)[attr(x, "locicol")],
-                          c("Fit", "Fst", "Fis"))
-    obj
+    if (!quiet) cat("... Done.\n")
+    dimnames(res) <- list(names(x)[-1], c("Fit", "Fst", "Fis"))
+    res
 }
