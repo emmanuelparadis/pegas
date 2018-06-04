c7ed83f0fbabd2c999b4b9e179a80ac92110dc00
emmanuelparadis <Emmanuel.Paradis@ird.fr>
2018-06-04 12:02:53 +0200

    bug fixed in Fst() see NEWS

--- pegas/R/Fst.R
+++ pegas/R/Fst.R
@@ -1,4 +1,4 @@
-## Fst.R (2018-04-20)
+## Fst.R (2018-06-04)
 
 ##   F-Statistics
 
@@ -12,28 +12,32 @@ Fst <- function(x, pop = NULL, quiet = TRUE)
     if (any(getPloidy(x) != 2))
         stop("Fst() requires diploid data")
 
+    NAMESX <- names(x)
     if (is.null(pop)) {
-        pop <- x$population
-        if (is.null(pop)) stop("no 'population' column in x")
+        ipop <- which(NAMESX == "population")
+        if (!length(ipop)) stop("no 'population' column in x")
     } else {
-        pop <- if (is.numeric(pop) && length(pop) == 1) x[, pop] else factor(pop)
+        if (is.numeric(pop) && length(pop) == 1) {
+            ipop <- pop
+        } else {
+            x$populationforthisanalysis <- factor(pop)
+            ipop <- length(x)
+        }
     }
 
     LOCI <- attr(x, "locicol")
     nloci <- length(LOCI)
-    x <- x[, LOCI]
-    x$pop <- factor(pop)
-    ipop <- nloci + 1
 
     ## 'p' is a matrix with alleles as columns and populations
     ##    as rows, and its entries are the counts
     ## 'h' is the same with the number of heterozygotes
 
     res <- matrix(0, nloci, 3)
+    dimnames(res) <- list(NAMESX[LOCI], c("Fit", "Fst", "Fis"))
 
     for (j in 1:nloci) {
         if (!quiet) cat("\rAnalyzing locus", j, "/", nloci)
-        Z <- x[, c(j, ipop)]
+        Z <- x[, c(LOCI[j], ipop)]
         Z <- na.omit(Z) # all n's are calculated locus-wise (2018-04-20)
         N <- nrow(Z)
         nBYpop <- tabulate(Z$pop)
@@ -68,6 +72,5 @@ Fst <- function(x, pop = NULL, quiet = TRUE)
         res[j, 3] <- 1 - sum(c)/sum(b + c)
     }
     if (!quiet) cat("... Done.\n")
-    dimnames(res) <- list(names(x)[-1], c("Fit", "Fst", "Fis"))
     res
 }
