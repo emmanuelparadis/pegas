82dfef02dc4faf1e641dcde5a91760ddfea69106
emmanuelparadis <Emmanuel.Paradis@ird.fr>
2019-02-04 19:19:09 +0100

    new Rst()

--- pegas/R/Fst.R
+++ pegas/R/Fst.R
@@ -1,8 +1,8 @@
-## Fst.R (2018-09-08)
+## Fst.R (2010-02-04)
 
 ##   F-Statistics
 
-## Copyright 2009-2018 Emmanuel Paradis
+## Copyright 2009-2019 Emmanuel Paradis
 
 ## This file is part of the R-package `pegas'.
 ## See the file ../DESCRIPTION for licensing issues.
@@ -70,6 +70,94 @@ Fst <- function(x, pop = NULL, quiet = TRUE, na.alleles = "")
         res[j, 1] <- 1 - sum(c)/sum(a + b + c)
         res[j, 2] <- sum(a)/sum(a + b + c)
         res[j, 3] <- 1 - sum(c)/sum(b + c)
+    }
+    if (!quiet) cat("... Done.\n")
+    res
+}
+
+Rst <- function(x, pop = NULL, quiet = TRUE, na.alleles = "")
+{
+    foo <- function(gr, g1, g2) {
+        o <- outer(gr, gr, function(x, y)
+            x == g1 & y == g2 | x == g2 & y == g1)
+        o[lower.tri(o)] | o[upper.tri(o)]
+    }
+
+    NAMESX <- names(x)
+    if (is.null(pop)) {
+        pop <- x$population
+        if (is.null(pop)) stop("no 'population' column in x")
+    } else {
+        if (length(pop) == 1) {
+            pop <- x[[pop]]
+        } else {
+            pop <- factor(pop)
+            if (length(pop) != nrow(x))
+                stop("length of 'pop' does not match number of rows in 'x'")
+        }
+    }
+    pop <- as.integer(pop)
+    LOCI <- attr(x, "locicol")
+    nloci <- length(LOCI)
+    res <- numeric(nloci)
+    names(res) <- NAMESX[LOCI]
+    Z <- loci2alleles(x)
+    storage.mode(Z) <- "double"
+    if (all(is.na(Z))) stop("no allele interpretable as number of repeats")
+
+    ploidy <- pegas:::.checkPloidy(x)
+
+    K <- length(unique(pop)) # Nb of pops
+    one2K <- 1:K
+    expr1 <- 2 / (K * (K - 1))
+
+    gr.diploid <- c(pop, pop)
+
+    with.na.alleles <- !identical(na.alleles, "")
+
+    b <- 0L
+    for (j in 1:nloci) {
+        if (!quiet) cat("\rAnalyzing locus", j, "/", nloci)
+        a <- b + 1L
+        b <- a + ploidy[j] - 1L
+        y <- Z[, a:b]
+        dim(y) <- NULL
+        gr <- if (ploidy[j] == 2) gr.diploid else rep(pop, ploidy[j])
+
+        ## drop missing data (null alleles, ...)
+        del <- is.na(y)
+        if (with.na.alleles) del <- y %in% na.alleles | del
+        if (any(del)) {
+            y <- y[!del]
+            gr <- gr[!del]
+        }
+
+        sqdf <- as.dist(outer(y, y, "-")^2) # squared differences
+        Nbypop <- tabulate(gr, K)
+
+        SW <- 0
+        for (i in one2K) {
+            if (!Nbypop[i]) next
+            o <- foo(gr, i, i)
+            SW <- SW + sum(sqdf[o]) / sum(o) # sum(o) is equal to (n*(n - 1))/2
+        }
+        SW <- SW / sum(Nbypop > 0)
+
+        SB <- 0
+        for (i1 in 1:(K - 1)) {
+            if (!Nbypop[i1]) next
+            for (i2 in (i1 + 1):K) {
+                if (!Nbypop[i2]) next
+                o <- foo(gr, i1, i2)
+                SB <- SB + sum(sqdf[o]) / sum(o)
+            }
+        }
+        SB <- SB * expr1
+
+        nbar <- mean(Nbypop)
+        denom <- sum(Nbypop) - 1
+        Sbar <- SW * (nbar - 1) / denom + SB * (nbar * (K - 1)) / denom
+        res[j] <- (Sbar - SW) / Sbar
     }
     if (!quiet) cat("... Done.\n")
     res
